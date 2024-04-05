3fb59af788ca9ebd99600e77a63a54a347ce3cb8
Pule <proceedingmc@gmail.com>
2024-04-05 14:18:34 +0200

    Testing

--- 0x01-react_intro/task_5/dashboard/src/App/App.test.js
+++ 0x01-react_intro/task_5/dashboard/src/App/App.test.js
@@ -1,8 +1,12 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App component', () => {
+  test('renders header text', () => {
+    render(<App />);
+    const headerElement = screen.getByText(/School dashboard/i);
+
+    expect(headerElement).toBeInTheDocument();
+  });
 });
