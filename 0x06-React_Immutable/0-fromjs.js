fd650d9985c442b2b7be535b18e1adcc87d2f846
Pule <proceedingmc@gmail.com>
2024-04-25 20:45:58 +0200

    0-fromjs.js

--- 0x06-React_Immutable/0-fromjs.js
+++ 0x06-React_Immutable/0-fromjs.js
@@ -1,13 +1,13 @@
 /* 0-fromjs.js*/
-//import fromJS function from immutable.js
+// Importing the `fromJS` function from the 'immutable' library.
+// `fromJS` is used to convert a plain JavaScript object into an Immutable.js data structure.
 import { fromJS } from 'immutable';
 
-// Define the getImmutableObject function
-const getImmutableObject = (object) => {
-    // Convert the input object into an immutable Map
-    const immutableMap = fromJS(object);
-    return immutableMap;
-};
-
-// Export the getImmutableObject function
-export default getImmutableObject;
+// Defining a function named `getImmutableObject` which takes an `object` parameter.
+// This function is responsible for converting a plain JavaScript object into an Immutable.js Map.
+export default function getImmutableObject(object) {
+  // Calling the `fromJS` function with the `object` parameter.
+  // This function converts the plain JavaScript object into an Immutable.js data structure.
+  // It returns an Immutable.js Map representing the converted object.
+  return fromJS(object);
+}
